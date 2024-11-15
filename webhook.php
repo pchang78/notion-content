dfe92f80370027b478d0355a373b83cafd51417b
Patrick Chang <pchang78@gmail.com>
2024-11-14 20:56:09 -0500

    Update webhook URL to use the page_id

--- webhook.php
+++ webhook.php
@@ -3,21 +3,21 @@
 require_once(dirname(__FILE__) . '/../../../wp-load.php');
 
 // Get the webhook_id from the URL
-$webhook_id = isset($_GET['webhook_id']) ? sanitize_text_field($_GET['webhook_id']) : '';
+$id = isset($_GET['id']) ? sanitize_text_field($_GET['id']) : '';
 
 // If no webhook_id is provided, exit
-if (empty($webhook_id)) {
-    wp_die('No webhook_id provided.');
+if (empty($id)) {
+    wp_die('No id provided.');
 }
 
 // Query the database for the matching webhook_id
 global $wpdb;
 $table_name = $wpdb->prefix . 'notion_content';
-$page_entry = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE webhook_id = %s", $webhook_id));
+$page_entry = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE page_id= %s", $id));
 
 // If no matching entry is found, exit
 if (!$page_entry) {
-    wp_die('Invalid webhook_id.');
+    wp_die('Invalid id.');
 }
 
 
