c12c61e789f472851798b48fc74b986557291839
Patrick Chang <pchang78@gmail.com>
2024-12-20 12:12:26 -0500

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -3,24 +3,33 @@
 require_once(dirname(__FILE__) . '/../../../wp-load.php');
 
 // Get the webhook_id from the URL
-$id = isset($_GET['id']) ? sanitize_text_field($_GET['id']) : '';
+$id = isset($_GET['id']) ? sanitize_text_field(wp_unslash($_GET['id'])) : '';
 
 // If no webhook_id is provided, exit
 if (empty($id)) {
     wp_die('No id provided.');
 }
 
-// Query the database for the matching webhook_id
-global $wpdb;
-$table_name = $wpdb->prefix . 'notion_content';
-$page_entry = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE page_id= %s", $id));
+// Query for posts with matching notion_page_id
+$args = array(
+    'post_type' => 'notion_content',
+    'meta_query' => array(
+        array(
+            'key' => 'notion_page_id',
+            'value' => $id,
+            'compare' => '='
+        )
+    ),
+    'posts_per_page' => 1
+);
 
-// If no matching entry is found, exit
-if (!$page_entry) {
+$query = new WP_Query($args);
+
+// If no matching post is found, exit
+if (!$query->have_posts()) {
     wp_die('Invalid id.');
 }
 
-
-notion_content_refresh_single_page($page_entry->page_id);
+notion_content_refresh_single_page($id);
 
 
